959aa2a7ba2267db013f9d2eca7ffc8b5898fdc1
igor <igor.podolak@gmail.com>
2025-04-09 22:00:43 +0200

    Enhanced jump points analysis with PCA weight space visualization

---
+++ loss_functions.py
@@ -0,0 +1,22 @@
+import torch
+from torch import nn
+
+
+class LabelSmoothingLoss(nn.Module):
+    def __init__(self, classes, smoothing=0.1):
+        super(LabelSmoothingLoss, self).__init__()
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+        self.classes = classes
+
+    def forward(self, pred, target):
+        pred = pred.log_softmax(dim=-1)
+        with torch.no_grad():
+            true_dist = torch.zeros_like(pred)
+            true_dist.fill_(self.smoothing / (self.classes - 1))
+            true_dist.scatter_(1, target.unsqueeze(1), self.confidence)
+        return torch.mean(torch.sum(-true_dist * pred, dim=-1))
+
+    def __repr__(self):
+        return "LabelSmoothingLoss(classes={}, smoothing={})".format(self.classes, self.smoothing)
+
--- utils.py
+++ utils.py
@@ -2,7 +2,8 @@ import asyncio
 import os
 from concurrent.futures import ThreadPoolExecutor
 
-# info asynchronous unlinking of several files
+# info asynchronous unlinking of several files; perhaps the direct use of os.remove,
+#  as well as asynchronous actions may speed up?
 async def remove_files_async(file_list):
     loop = asyncio.get_event_loop()
     with ThreadPoolExecutor() as executor:
@@ -14,3 +15,54 @@ async def remove_files_async(file_list):
 ### info to execute
 # file_list = [f"checkpoint_{i}.pth" for i in range(100)]
 # asyncio.run(remove_files_async(file_list))
+
+
+# info some simple dataloader and sampler functions
+
+def get_sampler_iter_state(dataloader):
+    """
+    Try to capture the state of a dataloader's sampler iterator.
+    This is inherently difficult in PyTorch.
+    """
+    # Check if we have access to the sampler
+    if not hasattr(dataloader, 'sampler'):
+        return None
+
+    sampler = dataloader.sampler
+
+    # Check if sampler is a RandomSampler (most common with shuffle=True)
+    if hasattr(sampler, 'generator'):
+        # If it has a generator, we can save its state
+        return {
+            'generator_state': sampler.generator.get_state() if sampler.generator else None
+        }
+
+    # For SequentialSampler, we don't need state
+    return None
+
+def init_train_dataloader_state(dataloader):
+    return {
+        # Current position information
+        'batch_idx': 0,  # Which batch we're on
+        'epoch': 0,  # Current epoch
+        # Sampler state (critical for reproducibility)
+        'sampler_iter_state': get_sampler_iter_state(dataloader),
+        # DataLoader configuration (for recreation)
+        'batch_size': dataloader.batch_size,
+        'shuffle': True,  # or whatever your setting is
+        'num_workers': dataloader.num_workers,
+        'pin_memory': dataloader.pin_memory,
+        'drop_last': dataloader.drop_last
+    }
+
+def init_val_dataloader_state(dataloader):
+    return {
+        # Similar structure but typically simpler since evaluation is often sequential
+        'batch_idx': 0,  # Usually reset for each evaluation
+        # DataLoader configuration
+        'batch_size': dataloader.batch_size,
+        'shuffle': False,  # Typically False for eval
+        'num_workers': dataloader.num_workers,
+        'pin_memory': dataloader.pin_memory,
+        'drop_last': dataloader.drop_last
+    }
